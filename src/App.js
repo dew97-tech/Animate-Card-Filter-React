d5cc7d54d4f524f821a0c007dac0d6fe8630c5b3
dew97-tech <dewmallick97.bd@gmail.com>
2022-06-03 13:53:23 +0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import "./App.css";
+import Movie from "./Movie";
+import Filter from "./Filter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [popular, setPopular] = useState([]);
+    const [filtered, setFiltered] = useState([]);
+    const [activeGenre, setActiveGenre] = useState(0);
+
+    useEffect(() => {
+        fetchPopular();
+    }, []);
+    const fetchPopular = async () => {
+        const data = await fetch(
+            "https://api.themoviedb.org/3/movie/popular?api_key=2717348bfb61632f343151a4bc33d863&language=en-US&page=1"
+        );
+        const movies = await data.json();
+        setPopular(movies.results);
+        setFiltered(movies.results);
+    };
+    return (
+        <div className="App">
+            <Filter
+                popular={popular}
+                setFiltered={setFiltered}
+                activeGenre={activeGenre}
+                setActiveGenre={setActiveGenre}
+            />
+            <motion.div animate={{ y: 100 }} className="popular-movies">
+                {filtered.map((movie) => {
+                    return <Movie key={movie.id} movie={movie} />;
+                })}
+            </motion.div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,13 @@
+const Movie = ({ movie }) => {
+    return (
+        <div>
+            <h2>{movie.title}</h2>
+            <img
+                src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path}
+                alt=""
+            />
+        </div>
+    );
+};
+
+export default Movie;
